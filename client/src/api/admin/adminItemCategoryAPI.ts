3a8d4474116ff73965e4496687e3d722e9437345
khang phung <duykhangpdk0701@gmail.com>
2023-02-21 00:13:02 +0700

    add .

--- client/src/api/admin/adminItemCategoryAPI.ts
+++ client/src/api/admin/adminItemCategoryAPI.ts
@@ -1,14 +1,14 @@
-import ICategory from "interfaces/Category";
+import { IAdminCategory } from "interfaces/Category";
 import axiosClient from "../axiosClient";
 
 const adminItemCategoryAPI = {
-  getListOfItemCategory: async (): Promise<ICategory[]> => {
+  getListOfItemCategory: async (): Promise<IAdminCategory[]> => {
     const url = "/api/v1/admin/item-category";
     const res = await axiosClient.get(url);
     return res.data;
   },
 
-  getItemCategoryBySlug: async (slug: string): Promise<ICategory> => {
+  getItemCategoryBySlug: async (slug: string): Promise<IAdminCategory> => {
     const url = `/api/v1/admin/item-category/${slug}`;
     const res = await axiosClient.get(url);
     return res.data;
